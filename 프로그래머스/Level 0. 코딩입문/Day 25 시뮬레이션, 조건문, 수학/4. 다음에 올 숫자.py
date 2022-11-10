f9652816eb3a586bfb0d30ddd070403abb1aad06
김주원 <kjwt1124@icloud.com>
2022-11-10 16:29:36 +0900

    Upload Programmmers Day25 source code 22.11.10 16:29

---
+++ 프로그래머스/Level 0. 코딩입문/Day 25 시뮬레이션, 조건문, 수학/4. 다음에 올 숫자.py
@@ -0,0 +1,19 @@
+'''
+Question : 등차수열 혹은 등비수열 common이 매개변수로 주어질 때, 마지막 원소 다음으로 올 숫자를 return 하도록 solution 함수를 완성해보세요.
+'''
+
+def solution(common):
+    check = None
+    num = 0
+    
+    if common[2] - common[1] == common[1] - common[0] : 
+        check = "plus"
+        num = common[2] - common[1]
+    else : 
+        check = "multiple"
+        num = common[2] / common[1]
+        
+    if check == "multiple":
+        return common[-1] * num
+    else : 
+        return common[-1] + num
