7971e231793b80d45c25f7212078ef81f0676f82
Kim Juwon <81630351+Kim-Ju-won@users.noreply.github.com>
2023-01-01 06:28:14 +0900

    [level 2] Title: H-Index, Time: 1.67 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42747. H－Index/H－Index.py
@@ -0,0 +1,18 @@
+def solution(citations):
+    
+    check_list = 10001*[0]
+    answer = 0
+    
+    for c in citations:
+        check_list[c] += 1
+    left = 0 
+    right = sum(check_list)
+    for i in range(1,10001):
+        left += check_list[i-1]
+        right -= check_list[i-1]
+        if right >= i and left <= i : 
+            answer = i
+        if i<=5 : 
+            print(left, right, i)
+    
+    return answer
