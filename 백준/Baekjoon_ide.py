c4b5ff8c370380941fb06c924301c11f08960f95
김주원 <kjwt1124@icloud.com>
2023-01-02 00:33:42 +0900

    Update README.md

--- 백준/Baekjoon_ide.py
+++ 백준/Baekjoon_ide.py
@@ -1,15 +1,21 @@
-import sys 
+import sys
 
-n = int(sys.stdin.readline())
-min_bag = [-1 for _ in range(n+1)]
+def solution(n):
+    if n == 1: 
+        return False 
+    for i in range(2,int(n**0.5)+1):
+        if n % i == 0 : 
+            return False
+    return True
 
-for i in range(1,n+1):
-    if i >=3 : 
-        temp1 = i//3 if i%3 == 0 else n+1
-        temp2 = i//5 if i%5 == 0 else n+1
-        temp3 = min_bag[i-3]+1 if min_bag[i-3] != -1 else n+1
-        temp4 = min_bag[i-5]+1 if min_bag[i-5] != -1 else n+1
-        min_bag[i] = min(temp1, temp2, temp3, temp4)
-        if min_bag[i] == n+1 : 
-            min_bag[i] = -1
-print(min_bag[n])
+prime_num = [0]*(123456*2+1)
+for i in range(2,len(prime_num)):
+    if solution(i): 
+        prime_num[i] = 1
+
+while True:
+    n = int(sys.stdin.readline())
+    if n == 0 : 
+        break
+    count = prime_num[n+1:2*n+1].count(1)
+    print(count)
--- 프로그래머스/lv1/42748. K번째수/K번째수.py
+++
@@ -1,8 +0,0 @@
-def solution(array, commands):
-    answer = []
-    for command in commands : 
-        i, j, k = command
-        temp = array[i-1:j]
-        temp.sort()
-        answer.append(temp[k-1])
-    return answer
