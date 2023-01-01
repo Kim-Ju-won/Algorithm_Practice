9682f14b4740c6b9c478aeb17f68797fd314401b
김주원 <kjwt1124@icloud.com>
2023-01-02 08:51:43 +0900

    Update README.md 23.01.02 08:51

--- 백준/Baekjoon_ide.py
+++ 백준/Baekjoon_ide.py
@@ -1,21 +1,11 @@
 import sys
 
-def solution(n):
-    if n == 1: 
-        return False 
-    for i in range(2,int(n**0.5)+1):
-        if n % i == 0 : 
-            return False
-    return True
+n = int(sys.stdin.readline())
+arr = [0]*10001
+for _ in range(n):
+    arr[int(sys.stdin.readline())]+=1
 
-prime_num = [0]*(123456*2+1)
-for i in range(2,len(prime_num)):
-    if solution(i): 
-        prime_num[i] = 1
-
-while True:
-    n = int(sys.stdin.readline())
-    if n == 0 : 
-        break
-    count = prime_num[n+1:2*n+1].count(1)
-    print(count)
+for i in range(10001):
+    if arr[i] > 0 : 
+        for j in range(arr[i]) :
+            print(i)
