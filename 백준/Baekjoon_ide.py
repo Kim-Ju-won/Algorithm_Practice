7782bc75748d37f852860b15e49b734b132e1270
김주원 <kjwt1124@icloud.com>
2023-01-02 18:47:31 +0900

    Update REAADME.md

--- 백준/Baekjoon_ide.py
+++ 백준/Baekjoon_ide.py
@@ -1,11 +1,7 @@
 import sys
 
-n = int(sys.stdin.readline())
-arr = [0]*10001
-for _ in range(n):
-    arr[int(sys.stdin.readline())]+=1
-
-for i in range(10001):
-    if arr[i] > 0 : 
-        for j in range(arr[i]) :
-            print(i)
+n =int(sys.stdin.readline())
+ans = 1
+for i in range(1,n+1):
+    ans*=i
+print(ans)
