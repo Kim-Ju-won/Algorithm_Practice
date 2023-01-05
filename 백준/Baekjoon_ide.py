299d2dc5ac82fd698ff5b134d1c9e080a790deb4
김주원 <kjwt1124@icloud.com>
2023-01-05 10:52:37 +0900

    Update BOJ README.md 23.01.05 10:52

--- 백준/Baekjoon_ide.py
+++ 백준/Baekjoon_ide.py
@@ -1,40 +1,38 @@
-def solution(numbers):
-    answer = []
-    count = 0
-    for num in numbers: 
-        if num == 0: 
-            count+=1
-        answer.append(str(num))
-    if count == len(numbers):
-        return '0'
-    answer.sort(
-        key=lambda x : 4*x,
-        reverse=True)
-   
-    return  ''.join(answer)
-'''
-    answer = []
-    for num in numbers: 
-        answer.append(str(num))
-    answer.sort(
-        key=lambda x : (x,-len(x)) if len(x) >= 3 else (
-            (x+x[0],-len(x)) if len(x) == 2 else (x*3,-len(x))),
-        reverse=True)
-    ans = ''
-    i = 0
-    while i < len(answer):
-        if i+1 < len(answer):
-            if answer[i]+answer[i][0] == answer[i+1]:
-                if answer[i]+answer[i+1] > answer[i+1]+answer[i]:
-                    ans += (answer[i]+answer[i+1])
-                else : 
-                    ans += (answer[i+1]+answer[i])
-                i+=1
-            else : 
-                ans+=answer[i]
-        else : 
-            ans+=answer[i]
-        i+=1
+import sys
 
-    return ans
-'''
+def draw_star(n):
+    ans =[[ '*' for _ in range(n)] for _ in range(n)]
+    if n == 3 : 
+        k = n//3
+        for i in range(k,k+k):
+            for j in range(k,k+k):
+                ans[i][j]=' '
+        return ans
+    else : 
+        k = n// 3
+        for i in range(0,n,k):
+            for j in range(0,n,k):
+                if i % n == k and j % n == k :  
+                    for l in range(i,i+k):
+                        for m in range(j,j+k):
+                            ans[l][m] =' '
+                else : 
+                    temp = draw_star(k)
+                    t1 = 0 
+                    for l in range(i,i+k):
+                        t2 = 0 
+                        for m in range(j,j+k):
+                            ans[l][m] =temp[t1][t2]
+                            t2+=1
+                        t1+=1
+        return ans
+
+
+
+n = int(sys.stdin.readline())
+
+ans = draw_star(n)
+for i in range(n):
+    for j in range(n):
+        print(ans[i][j], end = '')
+    print()
