39995cfea82164b548d1e0af8faf713ed663fb2d
Kim Juwon <81630351+Kim-Ju-won@users.noreply.github.com>
2023-01-07 10:11:01 +0900

    [level 2] Title: 위장, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42578. 위장/위장.py
@@ -0,0 +1,12 @@
+def solution(clothes):
+    answer = 1
+    clothes_dict = dict()
+    for cloth in clothes : 
+        if cloth[1] in clothes_dict : 
+            clothes_dict[cloth[1]]+=1
+        else : 
+            clothes_dict[cloth[1]]=1
+    for key in clothes_dict : 
+        answer*=(clothes_dict[key]+1)
+    answer-=1
+    return answer
