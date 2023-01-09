a1f1236b9d9e6c3348b8979b7fb1da8675e6051c
김주원 <kjwt1124@icloud.com>
2023-01-10 00:22:30 +0900

    Update BOJ README.md 2023.01.10 00:11

--- 백준/Baekjoon_ide.py
+++ 백준/Baekjoon_ide.py
@@ -1,31 +1,5 @@
-import sys 
+import sys
 
-while True : 
-    s = sys.stdin.readline().rstrip()
-    if s[0] =='.':
-        break
-    check = []
-    answer = 'yes'
-    for c in s : 
-        if c in ['(','[']:
-            check.append(c)
-        elif c in [')',']'] : 
-            if len(check) > 0 : 
-                if c == ')':
-                    if check[-1] =='(':
-                        check.pop()
-                    else : 
-                        answer='no'
-                        break
-                else : 
-                    if check[-1] =='[':
-                        check.pop()
-                    else : 
-                        answer='no'
-                        break
-            else : 
-                answer='no'
-                break
-    if len(check) > 0 : 
-        answer = 'no'
-    print(answer)
+n, m= tuple(map(int, sys.stdin.readline().split()))
+arr = list(map(int, sys.stdin.readline().split()))
+
