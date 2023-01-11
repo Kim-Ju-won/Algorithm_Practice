b3a894c7df09c483f3f559d4b7dfb6a0b5210b65
김주원 <kjwt1124@icloud.com>
2023-01-11 21:21:19 +0900

    Update BOJ README.md 2023.01.11 21:21

--- 백준/Baekjoon_ide.py
+++ 백준/Baekjoon_ide.py
@@ -1,20 +1,22 @@
 import sys
-n, m= tuple(map(int, sys.stdin.readline().split()))
 
+n, m = tuple(map(int, sys.stdin.readline().split()))
+arr = list(map(int, sys.stdin.readline().split()))
+arr_sum = [0]*n
+count = 0 
 
-grid = []
-
-def sequence(grid, n, m):
-    if len(grid) == m :
-        for i in range(len(grid)):
-            print(grid[i], end = ' ')
-        print()
+for i in range(n):
+    if i == 0 : 
+        arr_sum[0] = arr[0]
     else : 
-        for i in range(1,n+1):
-            if i not in grid :
-                temp = [] 
-                temp += grid
-                temp.append(i)
-                sequence(temp, n, m)
+        arr_sum[i] = arr_sum[i-1] + arr[i]
+        if arr_sum[i] % m == 0 :
+            count += 1
 
-sequence(grid, n, m)
+for i in range(n-1):
+    for j in range(i+1,n) : 
+        arr_sum[j] -= arr[i]
+        if arr_sum[j] % m == 0 : 
+            count +=1
+
+print(count)
