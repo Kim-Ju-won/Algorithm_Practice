fa24e299e760e69af19669ae3ec477cd7e01f003
김주원 <kjwt1124@icloud.com>
2023-01-18 23:02:27 +0900

    Update BOJ README.md 2023.01.18 23:02

--- 백준/Baekjoon_ide.py
+++ 백준/Baekjoon_ide.py
@@ -1,12 +1,4 @@
 import sys 
 
-t = int(sys.stdin.readline())
-
-for _ in range(t):
-    w, e = tuple(map(int, sys.stdin.readline().split()))
-    ans = 1 
-    for i in range(e,e-w,-1):
-        ans *= i
-    for i in range(1,w+1):
-        ans //= i 
-    print(ans)
+n = int(sys.stdin.readline())
+arr = [[ 0 for _ in range(n)] for _ in range(n)]
