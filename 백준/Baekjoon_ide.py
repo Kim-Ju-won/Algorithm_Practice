a3dc64a77827d9480029e71329b62d1b579d4f6b
김주원 <kjwt1124@icloud.com>
2023-01-28 23:52:03 +0900

    Update BOJ and Programmers README.md

--- 백준/Baekjoon_ide.py
+++ 백준/Baekjoon_ide.py
@@ -1,4 +1,22 @@
-import sys 
+import sys
 
-n = int(sys.stdin.readline())
-arr = [[ 0 for _ in range(n)] for _ in range(n)]
+n, s = tuple(map(int, sys.stdin.readline().split()))
+arr = list(map(int, sys.stdin.readline().split()))
+arr.sort()
+j = 1
+check_sum = arr[0]
+ans = sys.maxsize
+
+for i in range(n):
+    while j < n and check_sum < s :
+        check_sum += arr[j]
+        j += 1
+    if check_sum >= s : 
+        ans = min(ans, j-i)
+    check_sum -= arr[i]
+if ans != sys.maxsize: 
+    print(ans)
+
+else : 
+    print(0)
+
--- 프로그래머스/lv1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.py
+++
@@ -1,4 +0,0 @@
-def solution(price, money, count):
-    answer = -1
-
-    return answer
