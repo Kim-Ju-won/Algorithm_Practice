40bc8310710ef46820d860cc2d65a008d5cefd28
Kim Juwon <81630351+Kim-Ju-won@users.noreply.github.com>
2024-02-01 15:42:31 +0900

    Updata README.md

--- 백준/Baekjoon_ide.py
+++ 백준/Baekjoon_ide.py
@@ -1,15 +1,2 @@
-import sys
-
-INT_MAX = sys.maxsize
-
-n = int(sys.stdin.readline())
-graph = [INT_MAX]* (n) + [0]
-
-for i in range(n, 0, -1):
-    if i*3 <= n: 
-        graph[i] = min(graph[i+1]+1, graph[i*3]+1,graph[i*2]+1)
-    elif i*2 <= n : 
-        graph[i] = min(graph[i+1]+1, graph[i*2]+1)
-    elif i+1 <= n : 
-        graph[i] = graph[i+1]+1
-print(graph[1])
+print(
+)
