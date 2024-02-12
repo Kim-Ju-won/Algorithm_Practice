87a5e489f98458e3dc0338762971c459cb3a580c
Kim Juwon <81630351+Kim-Ju-won@users.noreply.github.com>
2024-02-12 23:45:58 +0900

    [level 0] Title: 배열 만들기 5, Time: 1.52 ms, Memory: 11.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181912. 배열 만들기 5/배열 만들기 5.py
@@ -0,0 +1,6 @@
+def solution(intStrs, k, s, l):
+    answer = []
+    for number in intStrs : 
+        if int(number[s:s+l]) > k : 
+            answer.append(int(number[s:s+l]))
+    return answer
