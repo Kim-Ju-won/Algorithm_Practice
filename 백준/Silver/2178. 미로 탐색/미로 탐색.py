4003d8f06c62f279be0e71821df8422b1ceca313
Kim Juwon <81630351+Kim-Ju-won@users.noreply.github.com>
2024-08-08 15:22:37 +0900

    [Silver I] Title: 미로 탐색, Time: 76 ms, Memory: 34112 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -2,36 +2,33 @@ import sys
 from collections import deque
 
 n, m = tuple(map(int, sys.stdin.readline().split()))
-maze= [ sys.stdin.readline().rstrip() for _ in range(n)]
-visited = [[ 0 for _ in range(m)] for _ in range(n)]
+maze = [sys.stdin.readline().rstrip() for _ in range(n)]
+visited = [[0 for _ in range(m)] for _ in range(n)]
 
-def BFS(n, m, maze): 
+
+def BFS(n, m, maze):
     q = deque()
-    i, j = 0,0
+    i, j = 0, 0
     visited[i][j] = 1
-    q.append((i,j))
-    count = 1
-    check_list = deque([(0,0)])
+    q.append((i, j))
     while q:
-        i,j= q.popleft()
-        if (i,j) == (n-1,m-1):
+        i, j = q.popleft()
+        if (i, j) == (n - 1, m - 1):
             break
-        if i+1 < n and visited[i+1][j] == 0 and maze[i+1][j] == '1':
-            visited[i+1][j] = 1
-            q.append((i+1,j))
-        if i-1 >= 0 and visited[i-1][j] == 0 and maze[i-1][j] == '1':
-            visited[i-1][j] = 1
-            q.append((i-1,j))
-        if j+1 < m and visited[i][j+1] == 0 and maze[i][j+1] == '1':
-            visited[i][j+1] = 1
-            q.append((i,j+1))
-        if j-1 >= 0 and visited[i][j-1] == 0 and maze[i][j-1] == '1':
-            visited[i][j-1] = 1
-            q.append((i,j-1))
-        if check_list and (i,j) == check_list[0]:
-            count+=1
-            check_list.popleft()
-            check_list.append(q[-1])
-    return count
+        if i + 1 < n and visited[i + 1][j] == 0 and maze[i + 1][j] == "1":
+            visited[i + 1][j] = visited[i][j] + 1
+            q.append((i + 1, j))
+        if i - 1 >= 0 and visited[i - 1][j] == 0 and maze[i - 1][j] == "1":
+            visited[i - 1][j] = visited[i][j] + 1
+            q.append((i - 1, j))
+        if j + 1 < m and visited[i][j + 1] == 0 and maze[i][j + 1] == "1":
+            visited[i][j + 1] = visited[i][j] + 1
+            q.append((i, j + 1))
+        if j - 1 >= 0 and visited[i][j - 1] == 0 and maze[i][j - 1] == "1":
+            visited[i][j - 1] = visited[i][j] + 1
+            q.append((i, j - 1))
+
+    return visited[n - 1][m - 1]
+
 
 print(BFS(n, m, maze))
