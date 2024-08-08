543bf5a667b49fdff7d16b3eab0a12d81763c0b2
Kim Juwon <81630351+Kim-Ju-won@users.noreply.github.com>
2024-08-08 14:27:12 +0900

    [Silver II] Title: DFS와 BFS, Time: 64 ms, Memory: 34948 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -1,64 +1,59 @@
 import sys
 from collections import deque
 
-n, m, v = tuple(map(int ,sys.stdin.readline().split()))
-edges = [ tuple(map(int, sys.stdin.readline().split())) for _ in range(m)]
-
-graph = [ list() for _ in range(n+1)]
-dfs_visited  = [ 0 for _ in range(n+1)]
-bfs_visited = [ 0 for _ in range(n+1)]
-dfs_count = 1
-
-for edge in edges : 
+n, m, s = tuple(map(int, sys.stdin.readline().split()))
+graph = [[] for _ in range(n + 1)]
+visited = [0 for _ in range(n + 1)]
+q = deque()
+count = 0
+edges = [tuple(map(int, sys.stdin.readline().split())) for _ in range(m)]
+for edge in edges:
     x, y = edge
     graph[x].append(y)
     graph[y].append(x)
 
-for i in range(1,n+1):
+for i in range(1, n + 1):
     graph[i].sort()
 
-def dfs(v, graph, dfs_visited):
-    global dfs_count
-    dfs_visited[v] = dfs_count
-    dfs_count += 1
-    for u in graph[v]:
-        if dfs_visited[u] == 0 : 
-            dfs(u, graph, dfs_visited)
 
-def bfs(v, graph, count, bfs_visited):
-    q = deque()
-    bfs_visited[v] = count
-    count+=1
-    q.append(v)
-    while q : 
-        v = q.popleft()
-        for u in graph[v]:
-            if bfs_visited[u] == 0 : 
-                bfs_visited[u] = count 
-                count+=1
-                q.append(u)
+def DFS(s):
+    global count
+    count += 1
+    visited[s] = count
+    for u in graph[s]:
+        if visited[u] == 0:
+            DFS(u)
 
 
-dfs(v, graph, dfs_visited)
-bfs(v, graph, 1, bfs_visited)
+DFS(s)
+DFS_visited = dict()
 
-dfs_order = dict()
-bfs_order = dict()
+for i, v in enumerate(visited):
+    if v > 0:
+        DFS_visited[v] = i
+        visited[i] = 0
 
-for i in range(1,len(dfs_visited)):
-    dfs_order[dfs_visited[i]] = i
-for i in range(1, len(bfs_visited)):
-    bfs_order[bfs_visited[i]] = i
+q.append(s)
+count = 1
+visited[s] = count
+while q:
+    u = q.popleft()
+    for w in graph[u]:
+        if visited[w] == 0:
+            q.append(w)
+            count += 1
+            visited[w] = count
 
-dfs_keys = list(dfs_order.keys())
-bfs_keys = list(bfs_order.keys())
-dfs_keys.sort()
-bfs_keys.sort()
+BFS_visited = dict()
 
-for key in dfs_keys:
-    if key != 0 : 
-        print(dfs_order[key], end=' ')
+for i, v in enumerate(visited):
+    if v > 0:
+        BFS_visited[v] = i
+        visited[i] = 0
+dkeys = sorted(list(DFS_visited.keys()))
+bkeys = sorted(list(BFS_visited.keys()))
+for i in dkeys:
+    print(DFS_visited[i], end=" ")
 print()
-for key in bfs_keys : 
-    if key != 0 : 
-        print(bfs_order[key], end=' ')
+for i in bkeys:
+    print(BFS_visited[i], end=" ")
