3a97466ffb98ab4965e02b86b9f0713c7888b83a
Kim Juwon <81630351+Kim-Ju-won@users.noreply.github.com>
2024-08-15 23:37:59 +0900

    [Bronze I] Title: 알고리즘 수업 - 피보나치 수 1, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/24416. 알고리즘 수업 － 피보나치 수 1/알고리즘 수업 － 피보나치 수 1.py
+++ 백준/Bronze/24416. 알고리즘 수업 － 피보나치 수 1/알고리즘 수업 － 피보나치 수 1.py
@@ -2,12 +2,24 @@ import sys
 
 n = int(sys.stdin.readline())
 
-def fibonacci(n,count):
-    f = [0] * (n+1)
-    f[1], f[2] = 1, 1
-    for i in range(3,n+1):
-        f[i] = f[i-1] + f[i-2]
-        count+=1
-    return count, f[n]
-count_dp, count_re = fibonacci(n,0)
-print(count_re,count_dp)
+
+def fibo1(n):
+    fibo = [0 for _ in range(n + 1)]
+    for i in range(1, n + 1):
+        if i == 1 or i == 2:
+            fibo[i] = 1
+        else:
+            fibo[i] = fibo[i - 1] + fibo[i - 2]
+    return fibo[n]
+
+
+def fibo2(n):
+    fibo = [0 for _ in range(n + 1)]
+    fibo[0] = 1
+    fibo[1] = 1
+    for i in range(3, n + 1):
+        fibo[i] = fibo[i - 1] + 1
+    return fibo[n]
+
+
+print(fibo1(n), fibo2(n))
