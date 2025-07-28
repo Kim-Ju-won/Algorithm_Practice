6f3fba1e86965d61926b74b13ffcc7c969097cdb
Kim Juwon <81630351+Kim-Ju-won@users.noreply.github.com>
2025-07-28 20:54:00 +0900

    [level 2] Title: [PCCP 기출문제] 3번 / 아날로그 시계, Time: 0.02 ms, Memory: 9.64 MB -BaekjoonHub

--- 프로그래머스/2/250135. ［PCCP 기출문제］ 3번 ／ 아날로그 시계/［PCCP 기출문제］ 3번 ／ 아날로그 시계.py
+++ 프로그래머스/2/250135. ［PCCP 기출문제］ 3번 ／ 아날로그 시계/［PCCP 기출문제］ 3번 ／ 아날로그 시계.py
@@ -1,20 +1,22 @@
-import math
+from fractions import Fraction
 
 def count_events(P, t1, t2):
-    k_min = math.ceil(t1 / P)
-    k_max = math.floor(t2 / P)
+    n, d = P.numerator, P.denominator
+    k_min = (t1 * d + n - 1) // n
+    k_max = (t2 * d) // n
     return max(0, k_max - k_min + 1)
-    
-def solution(h1, m1, s1, h2, m2, s2):
-    t1 = h1*3600 + m1*60 + s1
-    t2 = h2*3600 + m2*60 + s2
 
-    P_sm = 3600.0 / 59
-    P_sh = 43200.0 / 719
+def solution(h1, m1, s1, h2, m2, s2):
+
+    t1 = h1 * 3600 + m1 * 60 + s1
+    t2 = h2 * 3600 + m2 * 60 + s2
+
+    P_sm = Fraction(3600, 59)    
+    P_sh = Fraction(43200, 719)  
 
     cnt = count_events(P_sm, t1, t2) + count_events(P_sh, t1, t2)
     for T in (0, 43200):
         if t1 <= T <= t2:
             cnt -= 1
-    
+
     return cnt
