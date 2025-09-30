ed63269fb7e907b8dc83c96ace00042d765d328d
Kim Juwon <81630351+Kim-Ju-won@users.noreply.github.com>
2025-09-30 12:05:06 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 6.60 ms, Memory: 9.95 MB -BaekjoonHub

--- 프로그래머스/1/258712. 가장 많이 받은 선물/README.md
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/README.md
@@ -1,10 +1,10 @@
 # [level 1] 가장 많이 받은 선물 - 258712 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/258712?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/258712) 
 
 ### 성능 요약
 
-메모리: 10.9 MB, 시간: 6.18 ms
+메모리: 9.95 MB, 시간: 6.60 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 05일 13:13:02
+2025년 09월 30일 12:05:03
 
 ### 문제 설명
 
--- 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.py
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.py
@@ -1,56 +1,40 @@
 def solution(friends, gifts):
-    answer = 0
     friends_dict = dict()
-    friend_number = dict()
+    friends_checker = dict()
+    answer = 0 
+    
     for friend in friends : 
-        friends_dict[friend] = dict()
-        friend_number[friend] = {"give":0, "receive":0, "number":0}
-    for key in friends_dict: 
-        for friend in friends: 
-            if key != friend : 
-                friends_dict[key][friend] = 0 
+        friends_dict[friend] = {
+            "recieve":0,
+            "sent":0,
+            "ratio":0
+        }
+        friends_checker[friend] = dict()
+        for key in friends : 
+            friends_checker[friend][key] = 0
     
     for gift in gifts : 
-        give, get = gift.split()
-        friends_dict[give][get] +=1
-        friend_number[give]["give"] += 1
-        friend_number[get]["receive"] += 1
+        give, recieve = gift.split()
         
-    for key in friend_number : 
-        friend_number[key]["number"] = friend_number[key]["give"] - friend_number[key]["receive"]
-    
-    print(friend_number)
-    
-    max_num = -1
-    
-    for key in friends : 
-        temp = 0
-        for friend in friends_dict[key]:
-            if friends_dict[key][friend] > 0 or friends_dict[friend][key] > 0 : 
-                if friends_dict[key][friend] != friends_dict[friend][key] :
-                    if friends_dict[key][friend] > friends_dict[friend][key] :
-                        temp += 1
-                else : 
-                    if friend_number[key]["number"] > friend_number[friend]["number"]:
-                        temp += 1
-            else : 
-                if friend_number[key]["number"] > friend_number[friend]["number"]:
-                    temp += 1
-                    
-        if temp > max_num : 
-            max_num = temp
-                        
-                    
-                        
-                        
-            
-    ##선물 받은 기록이 있다면
-    ### 더 많은 선물을 준사람이 받음 +1                           
-    ## 받은 경우가 없다면
-    ### 선물지수가 더 작은 사람에게 선물 지수가 큰사람이 1개를 받음
-    ### 선물지수 = 이번달까지 자신이 친구들에게 준 선물의 수 - 받은 선물의 수 
-    ### 선물지수 같으면 주고 받지도 앖음           
-    
-    #### 선물을 가장 많이 받을 친구가 받을 선물의 수 
-    
+        # 선물지수 
+        friends_dict[give]["sent"] += 1
+        friends_dict[recieve]["recieve"] += 1
+        friends_checker[give][recieve] += 1
+        
+    for friend in friends : 
+        friends_dict[friend]["ratio"] = friends_dict[friend]["sent"] - friends_dict[friend]["recieve"]
+        
+    max_num = 0 
+    for A in friends : 
+        A_count = 0 
+        for B in friends : 
+            if A != B : 
+                if friends_checker[A][B] > friends_checker[B][A] :
+                    A_count += 1
+                    print(B, friends_checker[A][B],friends_checker[B][A] )
+                elif friends_checker[A][B] == friends_checker[B][A] : 
+                    if friends_dict[A]["ratio"] > friends_dict[B]["ratio"] : 
+                        A_count += 1
+                        print(B, friends_dict[A]["ratio"], friends_dict[B]["ratio"]  )
+        max_num = max(max_num, A_count)
     return max_num
